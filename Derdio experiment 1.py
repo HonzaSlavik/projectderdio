e76bf9da5816b59c491f1a131d52974dacd93a53
Honza Slavík <honza@ovax.cz>
2019-02-11 11:33:55 +0100

    Update Derdio experiment 1.py

--- Derdio experiment 1.py
+++ Derdio experiment 1.py
@@ -1,23 +1,21 @@
 from sense_hat import SenseHat
 import datetime
-sense = SenseHat()
 from time import sleep
+
+#object for work with SenseHat
+sense = SenseHat()
+
+#Doplň sem komentář
 sense.set_rotation(270)
+
+#Infinite loop (the cycle repeats itself)
 while True:
     datetime_object = datetime.datetime.now()
     print(datetime_object)
     humidity = sense.get_humidity()
-    print ("Humidity: %s %%rH" % humidity) 
+    print ("Humidity: %s %%rH" % humidity)
     temp = sense.get_temperature()
     print("Temperature: %s C" % temp)
     pressure = sense.get_pressure()
     print("Pressure: %s Millibars" % pressure)
     sleep(120)
-    
-
-
-
-
-
-
-
