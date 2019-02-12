f8c8ec63ff062d7917211178a77ea5aa712483a1
RustyStroin <47474290+RustyStroin@users.noreply.github.com>
2019-02-12 15:34:23 +0100

    Update Experiment Data 1.py

--- Experiment Data 1.py
+++ Experiment Data 1.py
@@ -1,16 +1,19 @@
-
+#Připojuji SenseHat čas...
 import time
 from sense_hat import SenseHat
 import ephem
 sense = SenseHat()
+#Zadávám souřadnice satelitu, který se nachází na modulu ZARYA
 iss = ephem.readtle("ISS (ZARYA)",
                     "1 25544U 98067A   19043.42555361  .00001350  00000-0  28477-4 0  9999",
                     "2 25544  51.6412 261.6420 0005384  21.1164  41.7177 15.53256867155878")
                     
-
+#Zadávám styl času lambda že 1sekunda = 1000
 msleep = lambda x: time.sleep(x / 1000.0)
+#Vytvářím soubor pro data
 DerdioLog = open("DerdioLog.csv", "w")
 DerdioLog.write("iss.Lat:;iss.Long;time;temp;humidity;pressure;")
+#Nekonečná smyčka a měření
 while True:
     cTime = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
     iss.compute()
@@ -24,9 +27,10 @@ while True:
     print("Temperature: %s C" % temp)
     DerdioLog.write("{0};{1};{2};{3};{4};{5};\n".format(iss.sublat, iss.sublong, cTime, temp, humidity, pressure));
     DerdioLog.flush();
-    msleep(3000)
+    #sleep 2minuty
+    msleep(120000)
    
-
+#uzavření souboru ale to se nikdy nestane
 DerdioLog.close();    
 
 
